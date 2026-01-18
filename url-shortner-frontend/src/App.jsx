0c265c7a7a05888ff53d46afcaf091d5b79c220f
prafulbcr7 <prafulasu@gmail.com>
2026-01-18 17:27:02 -0600

    Base Frontend UT.

---
+++ url-shortner-frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/signup' element={<RegisterPage/>}/>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
